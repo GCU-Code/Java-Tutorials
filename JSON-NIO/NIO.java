6b89f030a1ae07023c58dc80576756b8faea18ad
bricks-gcu <116583833+bricks-gcu@users.noreply.github.com>
2022-12-01 16:07:02 -0700

    Update NIO.java

--- JSON-NIO/NIO.java
+++ JSON-NIO/NIO.java
@@ -1,4 +1,3 @@
-```java
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -58,4 +57,3 @@ class Item
 	public int getQty() { return qty; }
 	public void setQty(int qty) { this.qty = qty; }
 }
-```
